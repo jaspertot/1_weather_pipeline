4d9521da5c61e6f8d4e6c09aed7a72f8370b0bb0
jaspertot <jaspercasile14@gmail.com>
2026-04-07 21:49:40 +0800

    clean.py done. Initialized enrich.py. Modified .gitignore.

--- transformation/clean.py
+++ transformation/clean.py
@@ -1,19 +1,66 @@
 import os, glob
 import pandas as pd
 from loguru import logger
+from datetime import datetime
 
+# Path definition
 log_path = os.path.join(os.path.dirname(__file__), '..', 'logs', 'ingestion.log')
 raw_path = output_path = os.path.join(os.path.dirname(__file__), '..', 'raw_data', '*.csv')
 
+duplicates = 0
+# Points to log file
 logger.add(log_path)
-list_of_files = glob.glob(raw_path) 
 
-latest_file_path = max(list_of_files, key=os.path.getctime)
-latest_file = os.path.basename(latest_file_path)
+def clean_data():
+    list_of_files = glob.glob(raw_path) 
+    latest_file_path = max(list_of_files, key=os.path.getctime)
+    latest_file = os.path.basename(latest_file_path)
+    logger.info(f"Recent CSV file is: {latest_file}")
 
-logger.info(f"Recent CSV file is: {latest_file}")
+    df = pd.read_csv(latest_file_path)
+    row_count = len(df)
 
-df = pd.read_csv(latest_file_path)
-row_count = len(df)
+    logger.info(f"Number of rows for {latest_file}: {row_count}")
 
-logger.info(f"Number of rows for {latest_file}: {row_count}")
+    # Cleaning Null Values
+    nn_columns = [
+        'city', 
+        'country', 
+        'lat', 
+        'lon', 
+        'temp_kelvin', 
+        'humidity', 
+        'dt'
+    ]
+
+    df = df.dropna(subset=nn_columns)
+
+    # Handling ground_level and sea_level pressure
+    df = df.fillna({
+        'ground_level_pressure': df['pressure'],
+        'sea_level_pressure': df['pressure']
+    })
+
+    # Filling visibility, if NULL with 0
+    df['visibility'] = df['visibility'].fillna(0)
+    logger.info("Null values were handled.")
+
+    # Adding fetched_at column
+    df['fetched_at'] = datetime.now()
+    logger.info("'fetched_at' column is created.")
+
+    # Convert some columns to their respective dtypes
+    df = df.astype({
+        'city': 'string',
+        'country': 'string',
+        'wind_gust': 'float64'
+    })
+    logger.info(f'Converted rows to their respective data types.')
+
+    # Removing duplicates
+    logger.info(f'Cleaned {df.duplicated(subset=['city', 'dt']).sum()} duplicates.')
+    df = df.drop_duplicates(subset=['city', 'dt'], keep='first')
+    logger.info(f'After dropping duplicates, the dataframe has {len(df)} rows')
+    
+    return df
+
---
+++ transformation/enrich.py
@@ -0,0 +1,8 @@
+from clean import clean_data
+
+def enrich_data():
+    df = clean_data()
+
+    return df
+
+print(enrich_data())
